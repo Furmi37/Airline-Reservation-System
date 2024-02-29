cd70059f1f31795084fc3098b0cd76b940b291b2
furmi <147946906+Furmi37@users.noreply.github.com>
2024-02-29 12:56:30 +0100

    #2_data

--- src/main/java/com/furmi/airline/controller/UserController.java
+++ src/main/java/com/furmi/airline/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.furmi.airline.controller;
 
 import com.furmi.airline.model.User;
+import com.furmi.airline.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserService userService;
     @GetMapping("/greeting")
     public String greeting() {
         return "hello";
@@ -16,5 +23,9 @@ public class UserController {
 //    public User getUser(@RequestParam(value = "name") String name) {
 //        return new User(name, 20);
 //    }
-
+    @GetMapping("/use/{email}")
+    public User getByEmail(@PathVariable String email){
+        log.info("Getting user by email {}", email);
+        return userService.getUserByEmail(email);
+    }
 }
