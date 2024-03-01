7a139b4de5d5d5995051f5931d37629b80cc88c9
Furmi37 <147946906+Furmi37@users.noreply.github.com>
2024-03-01 20:29:18 +0100

    #5_updated_ticket_controller (#3)

--- src/main/java/com/furmi/airline/controller/TicketController.java
+++ src/main/java/com/furmi/airline/controller/TicketController.java
@@ -1,7 +1,41 @@
 package com.furmi.airline.controller;
 
+import com.furmi.airline.model.Ticket;
+import com.furmi.airline.service.TicketService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@AllArgsConstructor
+@Slf4j
 public class TicketController {
+    private final TicketService ticketService;
+
+    @GetMapping("/ticket/{airlines}")
+    public List<Ticket> getByAirlines (@PathVariable String airlines){
+        log.info("Getting all tickets sold by {}", airlines);
+        return ticketService.getByAirlines(airlines);
+    }
+    @GetMapping("/ticket/{id}")
+    public Ticket getByTicketId (@PathVariable long id){
+        log.info("Getting by {}", id);
+        return ticketService.getById(id);
+    }
+
+    @GetMapping("/ticket")
+    public List<Ticket> getAll (){
+        log.info("Getting all tickets");
+        return ticketService.getAll();
+    }
+
+    @GetMapping("/ticket/{landAirport}")
+    public List<Ticket> getByLandAirport (@PathVariable String landAirport){
+        log.info("Getting by landing Airport {}", landAirport);
+        return ticketService.getByLandAirport(landAirport);
+    }
 }
