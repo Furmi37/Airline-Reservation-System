b07f55469fe217c662ddf411908bfbfe5d0b065e
Mateusz <147946906+Furmi37@users.noreply.github.com>
2024-03-04 11:42:48 +0100

    Controllers (#4)
    * #5_updated_ticket_controller
    
    * #7_updated_controllers

--- src/main/java/com/furmi/airline/controller/TicketController.java
+++ src/main/java/com/furmi/airline/controller/TicketController.java
@@ -4,9 +4,14 @@ import com.furmi.airline.model.Ticket;
 import com.furmi.airline.service.TicketService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -16,10 +21,16 @@ import java.util.List;
 public class TicketController {
     private final TicketService ticketService;
 
-    @GetMapping("/ticket/{airlines}")
+    @GetMapping("/ticket/airlines/{airlines}")
     public List<Ticket> getByAirlines (@PathVariable String airlines){
         log.info("Getting all tickets sold by {}", airlines);
         return ticketService.getByAirlines(airlines);
+    }
+
+    @GetMapping("/ticket/id/{id}")
+    public Ticket getByTicketId (@PathVariable long id){
+        log.info("Getting by id {}", id);
+        return ticketService.getById(id);
     }
 
     @GetMapping("/ticket")
@@ -28,9 +39,24 @@ public class TicketController {
         return ticketService.getAll();
     }
 
-    @GetMapping("/ticket/{landAirport}")
+    @GetMapping("/ticket/landAirport/{landAirport}")
     public List<Ticket> getByLandAirport (@PathVariable String landAirport){
         log.info("Getting by landing Airport {}", landAirport);
         return ticketService.getByLandAirport(landAirport);
     }
+
+    @PostMapping("/ticket")
+    public Long createTicket (@RequestBody Ticket ticket){
+        log.info("Creating ticket to {}", ticket.getLandAirport());
+        return ticketService.createTicket(ticket);
+    }
+    @PutMapping("/ticket/time")
+    public Long updateTicketStartandLandTime (@RequestParam long id, String startTime, String landTime){
+        log.info("Setting new start time {}", startTime);
+        Ticket ticket = ticketService.getById(id);
+        ticket.setStartTime(startTime);
+        ticket.setLandTime(landTime);
+        return ticketService.createTicket(ticket);
+    }
+
 }
